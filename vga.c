b085aeb3db919561e499fc86bef817dc5af48729
Avinav Bhandari <64446357+CanadianDesk@users.noreply.github.com>
2024-03-31 15:50:01 -0400

    VGA buffer is now working

--- vga.c
+++ vga.c
@@ -1,5 +1,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
 
 #define X_RES 320
 #define Y_RES 240
@@ -11,12 +13,12 @@
 /*==================STRUCTS==================*/
 
 typedef struct VGA {
-    int front_buffer;
-    int back_buffer;
-    short x_res;
-    short y_res;
-    int status;
-    int control;
+    volatile int front_buffer;
+    volatile int back_buffer;
+    volatile short x_res;
+    volatile short y_res;
+    volatile int status;
+    volatile int control;
 } VGA;
 
 /*==================FUNCTION PROTOTYPES==================*/
@@ -43,7 +45,6 @@ volatile int pixel_buffer_start;
 short int Buffer1[240][512];
 short int Buffer2[240][512];
 
-
 int main() 
 {
     //declare an instance of the VGA struct and initialize it
@@ -55,7 +56,9 @@ int main()
 
     //main loop
     while (1)
-    {
+    {   
+        //print buffer address
+        // printf("Writing buffer address: %x\n", pixel_buffer_start);
         //clear the screen
         clearScreen(BLACK);
         //draw a box
@@ -63,11 +66,8 @@ int main()
         //write a 1 to the vga front buffer to swap buffers
         vga->front_buffer = 1;
         //polling loop while waiting for the swap to happen
-        bool done = false;
-        while (!done)
-        {
-            done = !(vga->status & 0x01);
-        }
+        while (vga->status & 0x01);
+        pixel_buffer_start = vga->back_buffer;
     }
 
     return 0;
@@ -75,9 +75,9 @@ int main()
 
 void initializeVGA(VGA *vga)
 {
-    //set the resolution to 640 x 480
-    vga->x_res = X_RES;
-    vga->y_res = Y_RES;
+    //set the resolution to 320 x 240
+    assert(vga->x_res == 320);
+    assert(vga->y_res == 240);
 
     //set buffer 1 to the back buffer
     vga->back_buffer = (int) &Buffer1;
@@ -97,9 +97,7 @@ void initializeVGA(VGA *vga)
 void wait_for_v_sync(VGA *vga)
 {
     vga->front_buffer = 1; //start the synchronization process
-    bool done = false;
     while (vga->status & 0x01);
-
 }
 
 void plotPixel(int x, int y, short int color)
